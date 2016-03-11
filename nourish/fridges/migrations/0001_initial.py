fab0e24a5433426a7cadc4efc7d3da69ec59fa29
Harun Davood <davoodharun@gmail.com>
2016-03-11 00:58:04 -0500

    Storage Model added with migrations

---
+++ nourish/fridges/migrations/0001_initial.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='Storage',
+            fields=[
+                ('id', models.AutoField(primary_key=True, verbose_name='ID', auto_created=True, serialize=False)),
+            ],
+        ),
+    ]
--- nourish/fridges/tests.py
+++ nourish/fridges/tests.py
@@ -3,6 +3,7 @@ from django.test import TestCase
 from fridges.views import home_page
 from django.http import HttpRequest
 from django.template.loader import render_to_string
+from fridges.models import Storage
 
 class HomePageTest(TestCase):
     def test_rool_url_resolves_to_home_page_view(self):
@@ -31,3 +32,19 @@ class HomePageTest(TestCase):
             {'new_group_text': 'A new storage item'}
         )
         self.assertEqual(response.content.decode(), expected_html   )
+
+class StorageModelTest(TestCase):
+    
+    def test_save_retrieve_storage(self):
+        first_storage = Storage()
+        first_storage.text = 'Storage item 1'
+        first_storage.save()
+
+        second_storage = Storage()
+        second_storage.text = 'Storage item 2'
+        second_storage.save()
+        
+        saved_items = Storage.objects.all()
+        self.assertEqual(saved_items.count(), 2)
+        self.assertEqual(saved_items[0].text, 'Storage item 1')
+        self.assertEqual(saved_items[1].text, 'Storage item 2')
